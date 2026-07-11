a48a542d9c8069f4f970e9cf679922f1169fef1b
ZiramBayam <ramzialfito32@gmail.com>
2026-07-11 20:44:02 +0700

    feat: add component index exports

---
+++ frontend/components/index.ts
@@ -0,0 +1,9 @@
+// Main component exports
+export { default as TopAppBar } from "./TopAppBar";
+export { default as BottomNav } from "./BottomNav";
+export { default as WalletButton } from "./WalletButton";
+export { default as AllocationBuilder } from "./AllocationBuilder";
+export { default as SplitPreview } from "./SplitPreview";
+export { default as SplitBar, SplitBarLegend } from "./SplitBar";
+export { default as RecipientDashboard } from "./RecipientDashboard";
+export { default as TransactionStatus } from "./TransactionStatus";
