234a655af941d9c76e20654eb5610eff8427c9e3
ZiramBayam <ramzialfito32@gmail.com>
2026-07-15 13:54:54 +0700

    chore: remove deprecated frontend files

--- frontend/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-// UI Component exports
-export { default as Button } from "./Button";
-export { default as Input } from "./Input";
-export { default as Modal } from "./Modal";
-export { default as Spinner, PulseDot } from "./Spinner";
-
-// Icon exports
-export * from "./Icons";
